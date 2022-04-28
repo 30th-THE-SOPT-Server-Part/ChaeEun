e8ed174bc4e8ab7372144f82d043193a4815d4ec
chaeeunkim <rlacodms3447@naver.com>
2022-04-29 03:48:41 +0900

    [Chore] 코드 수정

--- seminar2/level2/src/api/index.ts
+++ seminar2/level2/src/api/index.ts
@@ -3,7 +3,6 @@ import express, { Router } from 'express';
 const router: Router = express.Router();
 
 router.use('/blog', require('./blog'));
-//router.use('/blog/:pageId', require('./blog')); 이거 안써도되나요
 router.use('/like', require('./like'));
 router.use('/signup', require('./signup'));
 router.use('/user', require('./user'));
